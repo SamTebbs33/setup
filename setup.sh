c851848d6b4370c97715ce0fe4a83015393297a8
Sam Tebbs <samuel.tebbs@yahoo.com>
2016-09-03 14:22:54 +0100

    Added chsh fish

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ sudo apt-get install vim git fish
 git clone http://github.com/SamTebbs33/vim .vim
 mkdir .config
 git clone http://github.com/SamTebbs33/fish .config/fish
+chsh -s /usr/bin/fish
 mkdir repos
