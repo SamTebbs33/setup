227c719239b81582721427730acea46f373a4441
Sam Tebbs <samuel.tebbs@yahoo.com>
2016-09-03 14:26:03 +0100

    Added git config commands

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 cd
 sudo apt-get install vim git fish
+git config --global user.email "samuel.tebbs@gmail.com"
+git config --global user.name "Sam Tebbs"
 git clone http://github.com/SamTebbs33/vim .vim
 mkdir .config
 git clone http://github.com/SamTebbs33/fish .config/fish
