932311bad5067370c1dee633c824d1715ac27b58
Sam Tebbs <samuel.tebbs@gmail.com>
2016-09-03 23:25:32 +0100

    Added missing --recursive to clone comands

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 cd
 sudo apt-get install vim git fish
-git clone http://github.com/SamTebbs33/vim .vim
+git clone --recursive http://github.com/SamTebbs33/vim .vim
 mkdir .config
-git clone http://github.com/SamTebbs33/fish .config/fish
+git clone --recursive http://github.com/SamTebbs33/fish .config/fish
 mkdir repos
