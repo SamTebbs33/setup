6c519e968767f658c2701d4b5c5cd81eb2b9b9a8
Sam Tebbs <samuel.tebbs@yahoo.com>
2016-10-31 14:28:25 +0000

    Added hub to list of packages to install

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
         alias get="brew install"
 fi
 cd
-get install vim git fish
+get install vim git fish hub
 git clone --recursive http://github.com/SamTebbs33/vim .vim
 mkdir .config
 git clone --recursive http://github.com/SamTebbs33/fish .config/fish
