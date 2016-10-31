cac6f081a66b0a6dfcceeea22e6aef0e8f003534
Sam Tebbs <samuel.tebbs@yahoo.com>
2016-10-31 12:18:47 +0000

    Added OS detection and dotfiles clone

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        alias get="sudo apt-get install"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        alias get="brew install"
+fi
 cd
-sudo apt-get install vim git fish
+get install vim git fish
 git clone --recursive http://github.com/SamTebbs33/vim .vim
 mkdir .config
 git clone --recursive http://github.com/SamTebbs33/fish .config/fish
+git clone --recursive http://github.com/SamTebbs33/dotfiles
 git config --global user.email "samuel.tebbs@gmail.com"
 git config --global user.name "Sam Tebbs"
 chsh -s /usr/bin/fish
